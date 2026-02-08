a1cd2eef659b523ed934a5dd5bc1dc8be3a0f74d
Jamuna S <erjamuna.s22@gmail.com>
2026-02-08 15:51:36 +0530

    Sum of between two numbers

---
+++ sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sum {
+    
+public static void main(String[] args) {
+    	Scanner scan=new Scanner(System.in);
+    	System.out.print("Enter the first number:");
+    	int a=scan.nextInt();
+    	System.out.print("Enter the second number:");
+    	int b=scan.nextInt();
+        for(int i=a;i<=b;i++) {
+        
+        		System.out.println(i);
+        	}
+        	
+       }
+}
